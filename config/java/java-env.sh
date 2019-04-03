86527c46913c04fd30e473e43ead05ab1203b79f
Matthias Deja <MadocDoyu@gmail.com>
2019-04-03 22:06:33 +0200

    Java 11 path fix.

--- config/java/java-env.sh
+++ config/java/java-env.sh
@@ -1,6 +1,6 @@
 export JAVA_HOME
 
 JAVA_08_HOME="`/usr/libexec/java_home -v 1.8`"
-JAVA_11_HOME="$HOME/bin/shared-meta/java/v11"
+JAVA_11_HOME="$HOME/bin/shared/java/v11"
 
 JAVA_HOME="$JAVA_11_HOME"
