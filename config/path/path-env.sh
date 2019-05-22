3ec96641b37b94447621d29b10f4243401f001b1
Matthias Deja <MadocDoyu@gmail.com>
2019-05-22 14:25:16 +0200

    Add user-scoped Python scripts to the path.

--- config/path/path-env.sh
+++ config/path/path-env.sh
@@ -4,3 +4,4 @@ PATH="$PATH:/usr/local/sbin:/opt/local/bin:/opt/local/sbin:/Library/Frameworks/P
 PATH="$PATH:$HOME/bin:$HOME/bin/shared:$HOME/bin/projects:$HOME/bin/apache-ant/bin"
 PATH="$PATH:$HOME/bin/shared/maven/bin:$HOME/bin/shared/prince:$HOME/bin/shared/sbt/bin:$HOME/bin/shared/scala/bin"
 PATH="$PATH:$FURY_HOME/bin"
+PATH="$PATH:$HOME/Library/Python/2.7/bin"
