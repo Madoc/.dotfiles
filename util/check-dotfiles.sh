e45ffe33f75579537320cd6b1765f28be7d5811c
Matthias Deja <MadocDoyu@gmail.com>
2019-06-19 10:24:52 +0200

    Add `.gradle`. Reorganize matching of expected file names.

--- util/check-dotfiles.sh
+++ util/check-dotfiles.sh
@@ -3,6 +3,71 @@
 # Checks all files in the home directory that start with a dot. Reports unexpected files. Run this regularly if you
 # want to keep your home directory tidy.
 
+is_expected() {
+  local base_name="$1"
+  case "${base_name}" in
+    '.' |\
+    '..' |\
+    '.CFUserTextEncoding' |\
+    '.CloudStation' |\
+    '.DS_Store' |\
+    '.Trash' |\
+    '.Xauthority' |\
+    '.adobe' |\
+    '.android' |\
+    '.aws' |\
+    '.bash_history' |\
+    '.bintray' |\
+    '.bundle' |\
+    '.cache' |\
+    '.config' |\
+    '.cups' |\
+    '.dirish' |\
+    '.docker' |\
+    '.dotfiles' |\
+    '.dotfiles-private' |\
+    '.dropbox' |\
+    '.eclipse' |\
+    '.gem' |\
+    '.gnupg' |\
+    '.gnupg_pre_2.1' |\
+    '.gradle' |\
+    '.hgignore_global' |\
+    '.history' |\
+    '.idea-build' |\
+    '.ideaLibSources' |\
+    '.iterm2' |\
+    '.ivy2' |\
+    '.kube' |\
+    '.lesshst' |\
+    '.local' |\
+    '.m2' |\
+    '.npm' |\
+    '.oh-my-zsh' |\
+    '.op' |\
+    '.oracle_jre_usage' |\
+    '.p2' |\
+    '.pdfbox.cache' |\
+    '.sbt' |\
+    '.scala_history' |\
+    '.scalac' |\
+    '.scalaide' |\
+    '.scdhistory' |\
+    '.sh_history' |\
+    '.ssh' |\
+    '.tooling' |\
+    '.travis' |\
+    '.uuid' |\
+    '.vim' |\
+    '.viminfo' |\
+    '.zinc' |\
+    '.zsh_history')
+    true;;
+  *)
+    false;;
+  esac
+}
+
 main() {
   for candidate_file in "${HOME}/".*; do
     base_name=$(basename "${candidate_file}")
@@ -11,19 +76,8 @@ main() {
       true
     elif [[ "${base_name}" == .zcompdump* ]]; then
       true
-    else
-      case "${base_name}" in
-      '.' | '..' | '.CFUserTextEncoding' | '.CloudStation' | '.DS_Store' | '.Trash' | '.Xauthority' | '.adobe' |\
-      '.android' | '.aws' | '.bash_history' | '.bintray' | '.bundle' | '.cache' | '.config' | '.cups' | '.dirish' |\
-      '.docker' | '.dotfiles' | '.dotfiles-private' | '.dropbox' | '.eclipse' | '.gem' | '.gnupg' | '.gnupg_pre_2.1' |\
-      '.hgignore_global' | '.history' | '.idea-build' | '.ideaLibSources' | '.iterm2' | '.ivy2' | '.kube' |\
-      '.lesshst' | '.local' | '.m2' | '.npm' | '.oh-my-zsh' | '.op' | '.oracle_jre_usage' | '.p2' | '.pdfbox.cache' |\
-      '.sbt' | '.scala_history' | '.scalac' | '.scalaide' | '.scdhistory' | '.sh_history' | '.ssh' | '.tooling' |\
-      '.travis' | '.uuid' | '.vim' | '.viminfo' | '.zinc' | '.zsh_history')
-        ;;
-      *)
-        echo "Unexpected file in home directory: ${candidate_file}" ;;
-      esac
+    elif ! is_expected "${base_name}"; then
+      echo "Unexpected file in home directory: ${candidate_file}"
     fi
   done
 }
