74c42e3438f292a0ee5a988878fe6ca4bdd0936c
Matthias Deja <MadocDoyu@gmail.com>
2019-07-05 09:42:13 +0200

    Add JDK 12.

--- config/java/java-env.sh
+++ config/java/java-env.sh
@@ -2,5 +2,6 @@ export JAVA_HOME
 
 JAVA_08_HOME="`/usr/libexec/java_home -v 1.8`"
 JAVA_11_HOME="$HOME/bin/shared/java/v11"
+JAVA_12_HOME="$HOME/bin/shared/java/v12"
 
-JAVA_HOME="$JAVA_11_HOME"
+JAVA_HOME="$JAVA_12_HOME"
--- config/java/java-rc.sh
+++ config/java/java-rc.sh
@@ -1,2 +1,3 @@
 alias j8="export JAVA_HOME=\"$JAVA_08_HOME\"; java -version"
 alias j11="export JAVA_HOME=\"$JAVA_11_HOME\"; java -version"
+alias j12="export JAVA_HOME=\"$JAVA_12_HOME\"; java -version"
